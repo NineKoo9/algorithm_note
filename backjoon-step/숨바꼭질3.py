c9d2e302033bae46f32b18443aa0ef019917dbdc
koo995 <107671886+koo995@users.noreply.github.com>
2025-06-03 14:05:37 +0900

    re: 숨바꼭질3

--- backjoon-step/숨바꼭질3.py
+++ backjoon-step/숨바꼭질3.py
@@ -23,5 +23,25 @@ def solution():
             visited[cur_point + 1] = 1
             q.append((cur_point + 1, second + 1))
 
+def solution2():
+    import heapq
+    N, K = map(int, input().split())
+    visited = [False] * 100001
 
-solution()
+    pq = []
+    visited[N] = True
+    heapq.heappush(pq, (0, N))
+    while pq:
+        second, cur_point = heapq.heappop(pq)
+        if cur_point == K:
+            print(second)
+            break
+        for i, next_point in enumerate([cur_point * 2, cur_point + 1, cur_point - 1]):
+            if not (0 <= next_point < 100001):
+                continue
+            if visited[next_point]:
+                continue
+            visited[next_point] = True
+            heapq.heappush(pq, (second if i == 0 else second + 1, next_point))
+
+solution2()
