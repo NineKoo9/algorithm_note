d5a5681ad68624c57f7424de6b252a55c66fc4d5
koo995 <107671886+koo995@users.noreply.github.com>
2025-07-01 15:00:24 +0900

    re: 가장 긴 증가하는 부분 수열

--- backjoon-step/가장 긴 증가하는 부분 수열.py
+++ backjoon-step/가장 긴 증가하는 부분 수열.py
@@ -55,5 +55,16 @@ def solution3():
 
     print(max(dp))
 
+def solution4():
+    N = int(input())
+    A = list(map(int, input().split()))
+    
+    dp = [1] * N
+    for i in range(N):
+        for j in range(i):
+            if A[j] < A[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    print(max(dp))
 
-solution3()
+
+solution4()
