06d86a2ec7b69820639549465f8a3029dda7d127
koo995 <107671886+koo995@users.noreply.github.com>
2025-07-01 18:02:02 +0900

    Re: 가장 큰 증가하는 부분 수열

--- backjoon-step/가장큰 증가하는 부분 수열.py
+++ backjoon-step/가장큰 증가하는 부분 수열.py
@@ -14,4 +14,15 @@ def solution():
                 dp[i] = max(dp[i], arrays[i])
     return max(dp)
 
-print(solution())
+def solution2():
+    N = int(input())
+    A = list(map(int, input().split()))
+    
+    dp = [A[i] for i in range(N)]
+    for i in range(N):
+        for j in range(i):
+            if A[j] < A[i]:
+                dp[i] = max(dp[i], dp[j] + A[i])
+    print(max(dp))
+
+solution2()
