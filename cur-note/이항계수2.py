272fdbe3e021a90fae6626547041598cbc04b769
koo995 <107671886+koo995@users.noreply.github.com>
2025-08-21 07:45:07 +0900

    re: 이항계수2

--- cur-note/이항계수2.py
+++ cur-note/이항계수2.py
@@ -10,4 +10,22 @@ def solution():
     factorial(N)
     print((dp[N] // dp[K] // dp[N-K]) % 10007 )
 
-solution()
+def solution2():
+    def factorial(n):
+        # 이것을 구하는 것을 최적화하자?
+        if n == 1 or n == 0:
+            return 1
+        if dp[n] != -1:
+            return dp[n]
+        dp[n] = n * factorial(n - 1)
+        return dp[n]
+    
+    # nCk 을 구해야하는데
+    n, k = map(int, input().split())
+    # 어떻게 구할까? 팩토리얼 계산이 필요하다.
+    dp = [-1] * (n + 1)
+    ans = factorial(n) // (factorial(k) * factorial(n - k))
+    print(ans % 10007)
+
+
+solution2()
