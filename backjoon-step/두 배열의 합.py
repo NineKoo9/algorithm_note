14753dfc85e6166577e6e50012339250c61ede3c
koo995 <gunhong951@gmail.com>
2025-11-26 12:40:15 +0900

    re: 두 배열의 합

--- backjoon-step/두 배열의 합.py
+++ backjoon-step/두 배열의 합.py
@@ -71,4 +71,71 @@ def solution2():
             count += (e - s)
     print(count)
 
-solution2()
+def solution3():
+    def my_bisect_left(arr, target):
+        s = 0
+        e = len(arr) - 1
+        ans = len(arr)
+        while s <= e:
+            m = (s + e) // 2
+            if arr[m] < target:
+                s = m + 1
+            else:  # arr[m] >= target
+                e = m - 1
+                ans = m
+        return ans
+
+    def my_bisect_right(arr, target):
+        s = 0
+        e = len(arr) - 1
+        ans = len(arr)
+        while s <= e:
+            m = (s + e) // 2
+            if arr[m] <= target:
+                s = m + 1
+            else: # arr[m] > target
+                e = m - 1
+                ans = m
+        return ans
+
+    T = int(input())
+
+    n = int(input())
+    A = list(map(int, input().split()))
+
+    m = int(input())
+    B = list(map(int, input().split()))
+
+    # 부배열의 합을 각각 더해서 T가 되는 쌍을 구해야한다.
+    # 먼저 A의 모든 부배열의 합을 구한다. 중복된 것도 있을 수 있으며 서로 다른 것으로 처리한다.
+    # 합을 빠르게 구하기 위해서 prefixSum을 구한다.
+    
+    # prefix_sum[i] 는 [0, i)구간의 모든 합이다.
+    prefix_sum_A = [0] * (n + 1)
+    for i in range(1, n + 1):
+        prefix_sum_A[i] = prefix_sum_A[i - 1] + A[i - 1]
+
+    prefix_sum_B = [0] * (m + 1)
+    for i in range(1, m + 1):
+        prefix_sum_B[i] = prefix_sum_B[i - 1] + B[i - 1]
+
+    sub_A_values = []
+    for i in range(n):
+        for j in range(i + 1, n + 1):
+            sub_A_values.append(prefix_sum_A[j] - prefix_sum_A[i])
+
+    sub_B_values = []
+    for i in range(m):
+        for j in range(i + 1, m + 1):
+            sub_B_values.append(prefix_sum_B[j] - prefix_sum_B[i])
+
+    ans = 0
+    sub_B_values.sort()
+    for sub_A_value in sub_A_values:
+        # 이제 B의 서브 합에서 targetvalue가 몇개 있는지 구하면 된다.
+        target_value = T - sub_A_value
+        sub_B_count = my_bisect_right(sub_B_values, target_value) - my_bisect_left(sub_B_values, target_value)
+        ans += sub_B_count
+    print(ans)
+    
+solution3()
