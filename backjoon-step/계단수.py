d8cd658053d5b193c58c49897cb7fa786d8a9709
koo995 <gunhong951@gmail.com>
2025-12-04 10:43:20 +0900

    re: 계단수

--- backjoon-step/계단수.py
+++ backjoon-step/계단수.py
@@ -25,4 +25,38 @@ def solution():
 
     print(sum(dp[N][i][(1 << 10) - 1] for i in range(10)) % int(1e9))
 
-solution()
+def solution2():
+    def dfs(idx, num, cur_state):
+        # 마지막에 도달했고
+        if idx == N - 1:
+            # 모든 수들이 나타났다면 1
+            if cur_state == (1 << 10) - 1:
+                return 1
+            # 그게 아니라면 0
+            else:
+                return 0
+        
+        if dp[idx][num][cur_state] != -1:
+            return dp[idx][num][cur_state]
+        
+        count = 0
+        for diff in [1, -1]:
+            next_num = num + diff
+            if not (0 <= next_num < 10):
+                continue
+            count += dfs(idx + 1, next_num, cur_state | 1 << next_num)
+        dp[idx][num][cur_state] = count
+        return dp[idx][num][cur_state]
+
+
+    N = int(input())
+
+    # dp[idx][n][state] 이면 idx에 n이 있고 지금까지 방문항 state라면 그 뒤로 방문해야할 지점들은 결정되어 있다.
+    dp = [[[-1 for _ in range(1 << 10)] for _ in range(10)] for _ in range(N)]
+    for first_num in range(1, 10):
+        dfs(0, first_num, 1 << first_num)
+
+    # 이렇게 되면 1을 처음 방문, 9을 처음 방문... 이 경우에 대한 수를 모두 더하는 것이 되겠군
+    print(sum(dp[0][n][1 << n] for n in range(1, 10) if dp[0][n][1 << n] != -1) % 1000000000)
+
+solution2()
